dce34069e0cbc3c3cf3903776203531242306aa6
JwoeBwiden <ariel1506br@gmail.com>
2025-01-05 14:56:30 +0200

    Setup

---
+++ app/src/main/java/Haim/Cohen/Moonchase/MainActivity.java
@@ -0,0 +1,58 @@
+package Haim.Cohen.Moonchase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Initializes Firebase, The Login Button, The User Textview, and FirebaseUser
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+
+    //On Create method, Connects the initialized widgets to their respective ID's on the screen
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logoutBTN);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        // If there's no User connected, the app takes you straight to the login page.
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), login.class);
+            startActivity(intent);
+            finish();
+        }
+        //Else, the User TextView shows your Email to show you that you are connected.
+        else {
+             textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getApplicationContext(), login.class);
+                    startActivity(intent);
+                    finish();
+            }
+        });
+
+    }
+}
